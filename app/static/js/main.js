09dcf0b2c6987ad015e741431f1f8c4955a3a7a0
Frank Mormino <frank.mormino@avlino.com>
2023-10-22 13:24:25 -0400

    css and js

---
+++ app/static/js/main.js
@@ -0,0 +1,7 @@
+// static/js/main.js
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Placeholder for any JavaScript functionality you'll add later
+    console.log('ScoreboardSync loaded successfully');
+});
+
