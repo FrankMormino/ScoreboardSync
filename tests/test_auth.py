9d19399f2c5588d08be0b7a2b0bb2260c7d1b161
Frank Mormino Jr <frank.mormino@avlino.com>
2023-10-22 14:20:52 -0400

    tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,12 +1,25 @@
 import unittest
-from app.auth import authenticate_user  # assuming authenticate_user is a function in your auth module
+from unittest.mock import patch
+from flask import url_for
+from app.auth import app
 
 class TestAuth(unittest.TestCase):
 
-    def test_authenticate_user(self):
-        # assuming authenticate_user returns True for successful authentication
-        self.assertTrue(authenticate_user('correct_username', 'correct_password'))
-        self.assertFalse(authenticate_user('wrong_username', 'wrong_password'))
+    def setUp(self):
+        app.config['TESTING'] = True
+        self.app = app.test_client()
+
+    @patch('app.auth.google')
+    def test_google_login(self, mock_google):
+        mock_google.authorize.return_value = 'Mock Google Authorization'
+        response = self.app.get(url_for('login_google'))
+        self.assertEqual(response.data.decode(), 'Mock Google Authorization')
+
+    @patch('app.auth.outlook')
+    def test_outlook_login(self, mock_outlook):
+        mock_outlook.authorize.return_value = 'Mock Outlook Authorization'
+        response = self.app.get(url_for('login_outlook'))
+        self.assertEqual(response.data.decode(), 'Mock Outlook Authorization')
 
 if __name__ == '__main__':
     unittest.main()
--- tests/test_calendar_integration.py
+++ tests/test_calendar_integration.py
@@ -1,14 +1,29 @@
 import unittest
-from app.calendar_integration import fetch_google_events  # assuming fetch_google_events is a function in your module
+from unittest.mock import patch
+from flask import url_for, session
+from app.calendar_integration import app
 
 class TestCalendarIntegration(unittest.TestCase):
 
-    def test_fetch_google_events(self):
-        events = fetch_google_events()
-        # assuming events is a list of event dictionaries
-        self.assertIsInstance(events, list)
-        if events:  # check the structure of an event if there are any events
-            self.assertIn('summary', events[0])
+    def setUp(self):
+        app.config['TESTING'] = True
+        self.app = app.test_client()
+
+    @patch('app.calendar_integration.google')
+    def test_fetch_google_events(self, mock_google):
+        with self.app.session_transaction() as sess:
+            sess['google_token'] = ('mock_token', '')
+        mock_google.get.return_value = 'Mock Google Events'
+        response = self.app.get(url_for('fetch_google_events'))
+        self.assertEqual(response.data.decode(), 'Mock Google Events')
+
+    @patch('app.calendar_integration.requests.get')
+    def test_fetch_outlook_events(self, mock_get):
+        with self.app.session_transaction() as sess:
+            sess['outlook_token'] = ('mock_token', '')
+        mock_get.return_value.json.return_value = 'Mock Outlook Events'
+        response = self.app.get(url_for('fetch_outlook_events'))
+        self.assertEqual(response.data.decode(), 'Mock Outlook Events')
 
 if __name__ == '__main__':
     unittest.main()
