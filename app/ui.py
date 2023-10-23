78da864dbf2b2368c61838271b6234428df2fb7b
Frank Mormino <frank.mormino@avlino.com>
2023-10-22 20:42:56 -0400

    Update ui.py with enhanced functionalities
    1. Imported necessary classes and modules at the top.
    2. Integrated form validation and flash messages in the link_calendar and notification_config route handlers.
    3. Added a placeholder get_schedule function call in the view_schedule route handler.
    4. Added a basic API endpoint for fetching events.
    5. Included a custom static file route handler, though Flask handles static files by default, this is for the advanced use case of static file versioning.
    6. Included a custom 404 error page handler.
    7. Set up basic logging configuration in the __main__ block.
    
    These updates provide a clearer structure for the ui.py file, enhance the user experience with flash messages, improve error handling with a custom 404 page, and provide basic API and logging functionalities.

--- app/ui.py
+++ app/ui.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify, send_from_directory
+from forms import LinkCalendarForm, NotificationConfigForm  # Import your actual form classes from forms.py
+import logging
 
 app = Flask(__name__)
 
@@ -8,25 +10,46 @@ def home():
 
 @app.route('/link_calendar', methods=['GET', 'POST'])
 def link_calendar():
-    if request.method == 'POST':
+    form = LinkCalendarForm(request.form)
+    if request.method == 'POST' and form.validate():
         # Process the submitted form data
         # ...
+        flash('Calendar linked successfully!', 'success')
         return redirect(url_for('home'))
-    return render_template('link_calendar.html')
+    return render_template('link_calendar.html', form=form)
 
 @app.route('/notification_config', methods=['GET', 'POST'])
 def notification_config():
-    if request.method == 'POST':
+    form = NotificationConfigForm(request.form)
+    if request.method == 'POST' and form.validate():
         # Process the submitted form data
         # ...
+        flash('Configuration saved successfully!', 'success')
         return redirect(url_for('home'))
-    return render_template('notification_config.html')
+    return render_template('notification_config.html', form=form)
 
 @app.route('/view_schedule')
 def view_schedule():
     # Fetch the synchronized sports schedule from your data store
-    # ...
+    # Assume get_schedule is a function that fetches the schedule from your data store
+    schedule = get_schedule()
     return render_template('view_schedule.html', schedule=schedule)
 
+@app.route('/api/events', methods=['GET'])
+def api_events():
+    # Assume get_events is a function that fetches events from your data store
+    events = get_events()
+    return jsonify(events)
+
+@app.route('/static/<path:filename>')
+def custom_static(filename):
+    return send_from_directory('static', filename)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
     app.run()
+
