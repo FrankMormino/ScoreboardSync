e95c0da9a8acb614aa6531173f80eb6fef786cd3
Frank Mormino Jr <frank.mormino@avlino.com>
2023-10-23 11:44:33 -0400

    fix location

--- tests/generate_calendar_events.sh
+++ tests/generate_calendar_events.sh
@@ -19,14 +19,13 @@ function generate_event() {
   local sport
   local event_type
   name=("Andrew" "Frankie" "Dominic" "Adelina" "Mackenzie" "Kalee" "Avery" "Joey" "Olivia" "Emma")
-  #sport=("basketball" "baseball" "soccer" "softball" "volleyball" "football" "hockey" "tennis")
   sport=("🏀 basketball" "⚾ baseball" "⚽ soccer" "🥎 softball" "🏐 volleyball" "🏈 football" "🏒 hockey" "🎾 tennis")
   event_type=("Practice" "Game" "Match" "Scrimmage" "Tournament" "Clinic" "Camp" "Tryout")
 
   local random_name
   local random_sport
   local random_event_type
-  local random_location
+  local random_location  # Move the location inside the function
 
   random_name=${name[$RANDOM % ${#name[@]}]}
   random_sport=${sport[$RANDOM % ${#sport[@]}]}
@@ -36,7 +35,7 @@ function generate_event() {
   emoji=$(echo "$random_sport" | cut -d' ' -f1)
   sport_name=$(echo "$random_sport" | cut -d' ' -f2-)
 
-  case "$random_sport" in
+  case "$sport_name" in
     "basketball")
       random_location="Toms River East Little League, 2195 Windsor Ave, Toms River, NJ 08753, USA"
       ;;
@@ -52,15 +51,24 @@ function generate_event() {
     "volleyball")
       random_location="Lake Riviera Middle School, 171 Beaverson Blvd, Brick Township, NJ 08723, USA"
       ;;
+    "football")
+      random_location="Football Stadium, 456 Stadium Dr, Stadium City, NJ 54321, USA"
+      ;;
+    "hockey")
+      random_location="Hockey Arena, 789 Ice Rink Rd, Ice City, NJ 67890, USA"
+      ;;
+    "tennis")
+      random_location="Tennis Court, 101 Racket St, Racketville, NJ 13579, USA"
+      ;;
     *)
-      random_location="Toms River East Little League, 2195 Windsor Ave, Toms River, NJ 08753, USA"
+      random_location="Default Location"
       ;;
   esac
 
-  #echo "{\"kind\":\"calendar#event\",\"etag\":\"$(cat /dev/urandom | tr -dc '0-9' | fold -w 16 | head -n 1)\",\"id\":\"event-id-$RANDOM\",\"status\":\"confirmed\",\"htmlLink\":\"https://www.google.com/calendar/event?eid=event-id-$RANDOM\",\"created\":\"$(date -Iseconds)\",\"updated\":\"$(date -Iseconds)\",\"summary\":\"$random_name 🏈 $random_sport $random_event_type\",\"location\":\"$random_location\",\"creator\":{\"email\":\"$(generate_email)\",\"self\":true},\"organizer\":{\"email\":\"$(generate_email)\",\"self\":true},\"start\":{\"dateTime\":\"2023-10-$(shuf -i 24-28 -n 1)T$(shuf -i 10-19 -n 1):00:00-04:00\",\"timeZone\":\"America/New_York\"},\"end\":{\"dateTime\":\"2023-10-$(shuf -i 24-28 -n 1)T$(shuf -i 10-19 -n 1):00:00-04:00\",\"timeZone\":\"America/New_York\"},\"iCalUID\":\"event-id-$RANDOM@google.com\",\"sequence\":0,\"reminders\":{\"useDefault\":true},\"eventType\":\"default\"},"
-  echo "{\"kind\":\"calendar#event\",\"etag\":\"$(cat /dev/urandom | tr -dc '0-9' | fold -w 16 | head -n 1)\",\"id\":\"event-id-$RANDOM\",\"status\":\"confirmed\",\"htmlLink\":\"https://www.google.com/calendar/event?eid=event-id-$RANDOM\",\"created\":\"$(date -Iseconds)\",\"updated\":\"$(date -Iseconds)\",\"summary\":\"$random_name $emoji $random_event_type $sport_name\",\"location\":\"$random_location\",\"creator\":{\"email\":\"$(generate_email)\",\"self\":true},\"organizer\":{\"email\":\"$(generate_email)\",\"self\":true},\"start\":{\"dateTime\":\"2023-10-$(shuf -i 24-28 -n 1)T$(shuf -i 10-19 -n 1):00:00-04:00\",\"timeZone\":\"America/New_York\"},\"end\":{\"dateTime\":\"2023-10-$(shuf -i 24-28 -n 1)T$(shuf -i 10-19 -n 1):00:00-04:00\",\"timeZone\":\"America/New_York\"},\"iCalUID\":\"event-id-$RANDOM@google.com\",\"sequence\":0,\"reminders\":{\"useDefault\":true},\"eventType\":\"default\"},"
-
+  #echo "{\"kind\":\"calendar#event\",\"etag\":\"$(cat /dev/urandom | tr -dc '0-9' | fold -w 16 | head -n 1)\",\"id\":\"event-id-$RANDOM\",\"status\":\"confirmed\",\"htmlLink\":\"https://www.google.com/calendar/event?eid=event-id-$RANDOM\",\"created\":\"$(date -Iseconds)\",\"updated\":\"$(date -Iseconds)\",\"summary\":\"$random_name $emoji $random_event_type $sport_name\",\"location\":\"$random_location\",\"creator\":{\"email\":\"$(generate_email)\",\"self\":true},\"organizer\":{\"email\":\"$(generate_email)\",\"self\":true},\"start\":{\"dateTime\":\"2023-10-$(shuf -i 24-28 -n 1)T$(shuf -i 10-19 -n 1):00:00-04:00\",\"timeZone\":\"America/New_York\"},\"end\":{\"dateTime\":\"2023-10-$(shuf -i 24-28 -n 1)T$(shuf -i 10-19 -n 1):00:00-04:00\",\"timeZone\":\"America/New_York\"},\"iCalUID\":\"event-id-$RANDOM@google.com\",\"sequence":0,\"reminders\":{\"useDefault\":true},\"eventType\":\"default\"},"
+  echo "{\"kind\":\"calendar#event\",\"etag\":\"$(cat /dev/urandom | tr -dc '0-9' | fold -w 16 | head -n 1)\",\"id\":\"event-id-\$RANDOM\",\"status\":\"confirmed\",\"htmlLink\":\"https://www.google.com/calendar/event?eid=event-id-\$RANDOM\",\"created\":\"$(date -Iseconds)\",\"updated\":\"$(date -Iseconds)\",\"summary\":\"$random_name $emoji $random_event_type $sport_name\",\"location\":\"$random_location\",\"creator\":{\"email\":\"$(generate_email)\",\"self\":true},\"organizer\":{\"email\":\"$(generate_email)\",\"self\":true},\"start\":{\"dateTime\":\"2023-10-\$(shuf -i 24-28 -n 1)T\$(shuf -i 10-19 -n 1):00:00-04:00\",\"timeZone\":\"America/New_York\"},\"end\":{\"dateTime\":\"2023-10-\$(shuf -i 24-28 -n 1)T\$(shuf -i 10-19 -n 1):00:00-04:00\",\"timeZone\":\"America/New_York\"},\"iCalUID\":\"event-id-\$RANDOM@google.com\",\"sequence\":0,\"reminders\":{\"useDefault\":true},\"eventType\":\"default\"},"
 }
+
 
 # Ask for the number of events
 read -r -p "Enter the number of events you'd like to create: " num_events
@@ -77,7 +85,7 @@ done
 # Save the events to a JSON file
 echo "[" >>calendar_events.json
 # Consider using { cmd1; cmd2; } >> file instead of individual redirects.
-echo "${events[@]}" | sed 's/,$//' >>calendar_events.json  # Remove trailing comma
+echo "${events[@]}" | sed "s/,$//" >>calendar_events.json  # Remove trailing comma
 echo "]" >>calendar_events.json
 
 
