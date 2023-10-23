2c182fc94dbb6b8d15ce908700bcae0d4a03df3e
Frank Mormino Jr <frank.mormino@avlino.com>
2023-10-22 20:37:45 -0400

    event filtering

--- app/event_filtering.py
+++ app/event_filtering.py
@@ -1,3 +1,4 @@
+from app.calendar_integration import fetch_google_events, fetch_outlook_events
 import re
 
 def filter_events(events):
@@ -9,10 +10,13 @@ def filter_events(events):
             filtered_events.append(event)
     return filtered_events
 
-# Assume google_events and outlook_events are the lists of events retrieved from Google and Outlook
-google_events = fetch_google_events()
-outlook_events = fetch_outlook_events()
+def fetch_and_filter_events():
+    # Assume google_events and outlook_events are the lists of events retrieved from Google and Outlook
+    google_events = fetch_google_events()
+    outlook_events = fetch_outlook_events()
 
-# Filter the events
-filtered_google_events = filter_events(google_events)
-filtered_outlook_events = filter_events(outlook_events)
+    # Filter the events
+    filtered_google_events = filter_events(google_events)
+    filtered_outlook_events = filter_events(outlook_events)
+
+    return filtered_google_events, filtered_outlook_events
